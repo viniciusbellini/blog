c5c2ad8d1b6766527d5106acb8d5b3f62c84bfa4
Vinicius Bellini <viniciusbellini@gmail.com>
2019-03-27 17:27:02 -0300

    first commit

---
+++ src/test/kotlin/br/com/bellini/blog/IntegrationTests.kt
@@ -0,0 +1,20 @@
+package br.com.bellini.blog
+
+import org.junit.Assert
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests (@Autowired val restTemplate: TestRestTemplate){
+
+    @Test
+    fun `Assert blog page title, content and status code`(){
+        val entity = restTemplate.getForEntity<String>("/")
+        Assert.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+}
